66c54f9068598c62218a8f5981b59847ab5d1eb6
shshwdr <cs.vivian04@gmail.com>
2018-05-10 02:06:04 -0700

    setup, can draw rainbow

---
+++ Assets/LoopPartMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopPartMusic : MonoBehaviour {
+	AudioSource audioSource;
+
+	public float startPoint = 60+41;
+	public float endPoint = 60+50;
+
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.time = startPoint;
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (endPoint!=-1 && audioSource.time >= endPoint) {
+			audioSource.time = startPoint;
+		}
+	}
+}
---
+++ Assets/PathCreator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(LineRenderer))]
+public class PathCreator : MonoBehaviour {
+	private LineRenderer lineRenderer;
+	public List<Vector3> points = new List<Vector3> ();
+	// Use this for initialization
+	void Start () {
+		lineRenderer = GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			//Debug.Log ("fire!");
+			points.Clear ();
+		}
+		if (Input.GetButton ("Fire1")) {
+			Vector3 mouseLoc = Camera.main.ScreenToWorldPoint (Input.mousePosition) + new Vector3 (0,0,10.0f);
+			//Debug.Log ("mouseLoc" + mouseLoc);
+			if (DistanceToLastPoint (mouseLoc)) {
+				//Debug.Log ("draw it");
+				points.Add (mouseLoc);
+				lineRenderer.positionCount = points.Count;
+				lineRenderer.SetPositions (points.ToArray ());
+			}
+		}
+	}
+
+	bool DistanceToLastPoint(Vector3 point){
+		return true;
+	}
+}
